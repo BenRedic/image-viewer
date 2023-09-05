5aba410a4f638503df8bbec5bdebea66fd6d254d
BenRedic <137050124+BenRedic@users.noreply.github.com>
2023-09-05 18:05:18 +0200

    routerlink figured out

--- image-app/src/app/app.module.ts
+++ image-app/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FileComponent } from './file/file.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    LandingPageComponent,
+    FileComponent,
+  ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { }
+      { path: '', component: LandingPageComponent },
+      { path: 'file', component: FileComponent },
     ]),
   ],
   providers: [],
